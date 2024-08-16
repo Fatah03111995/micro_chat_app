bc2570a432ce6c762cf4f89429bf0c9e59d7d78e
Abdul Fatah <fatah03111995@gmail.com>
2024-08-16 14:54:30 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# micro_chat_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/abdfatah/micro_chat_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.abdfatah.micro_chat_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
