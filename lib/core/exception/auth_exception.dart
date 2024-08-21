49fd2bd21b4104665cca037d15487cc655c73ad9
Abdul Fatah <fatah03111995@gmail.com>
2024-08-21 15:43:37 +0700

    fix : add return to toString method

--- lib/core/exception/auth_exception.dart
+++ lib/core/exception/auth_exception.dart
@@ -6,16 +6,19 @@ class AuthException implements Exception {
   String toString() {
     switch (message) {
       case 'invalid-credential':
-        const AuthException(message: 'wrong email or password');
+        return 'wrong email or password';
+
       case 'user-doesn\'t-exist':
-        const AuthException(message: 'email is not registered yet');
+        return 'email is not registered yet';
+
       case 'email-already-used':
-        const AuthException(message: 'email-already-used');
+        return 'email-already-used';
+
       case 'username-already-used':
-        const AuthException(message: 'username already used');
+        return 'username already used';
+
       default:
-        AuthException(message: message);
+        return message;
     }
-    return super.toString();
   }
 }
