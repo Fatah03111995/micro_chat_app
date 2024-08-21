bed6d3eadfe6aec92d4c860a8dfaa2216798b6f8
Abdul Fatah <fatah03111995@gmail.com>
2024-08-21 16:46:53 +0700

    feat : auth data provider register methode

--- lib/core/data_provider/auth_data_provider.dart
+++ lib/core/data_provider/auth_data_provider.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:micro_chat_app/core/env/env.dart';
 import 'package:http/http.dart' as http;
+import 'package:path/path.dart' as path;
 
 class AuthDataProvider {
   static Future<http.Response> login({
@@ -16,6 +18,34 @@ class AuthDataProvider {
     final response = await http.post(url,
         body: jsonEncode(data), headers: {"Content-Type": "application/json"});
 
+    return response;
+  }
+
+  static Future<http.StreamedResponse> register({
+    required String firstName,
+    required String lastName,
+    required String userName,
+    required String email,
+    required String password,
+    required String fcmToken,
+    required File photoProfilePath,
+  }) async {
+    final url = Uri.parse('${Env.baseEndpoint}/auth/register');
+    final request = http.MultipartRequest('POST', url);
+    request.files.add(http.MultipartFile(
+        'picture',
+        http.ByteStream(photoProfilePath.openRead()),
+        await photoProfilePath.length(),
+        filename: path.basename(photoProfilePath.path)));
+
+    request.fields["firstName"] = firstName;
+    request.fields["lastName"] = lastName;
+    request.fields["userName"] = userName;
+    request.fields["email"] = email;
+    request.fields["password"] = password;
+    request.fields["fcmToken"] = fcmToken;
+
+    final response = await request.send();
     return response;
   }
 }
