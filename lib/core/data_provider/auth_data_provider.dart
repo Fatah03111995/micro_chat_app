801c61c6e8e45eaf717da4ca739b537fb2134378
Abdul Fatah <fatah03111995@gmail.com>
2024-08-21 15:45:01 +0700

    feat : data provider for login

--- lib/core/data_provider/auth_data_provider.dart
+++ lib/core/data_provider/auth_data_provider.dart
@@ -1,11 +1,10 @@
 import 'dart:convert';
 
 import 'package:micro_chat_app/core/env/env.dart';
-import 'package:micro_chat_app/core/models/user_model.dart';
 import 'package:http/http.dart' as http;
 
 class AuthDataProvider {
-  static Future<UserModel> login({
+  static Future<http.Response> login({
     required String email,
     required String password,
   }) async {
@@ -14,31 +13,9 @@ class AuthDataProvider {
       "password": password,
     };
     final url = Uri.parse('${Env.baseEndpoint}/auth/login');
-    print(url);
     final response = await http.post(url,
         body: jsonEncode(data), headers: {"Content-Type": "application/json"});
 
-    if (response.statusCode != 200) {
-      final body = jsonDecode(response.body);
-      throw body["message"] ?? 'error-login';
-    }
-
-    final Map<String, dynamic> body = jsonDecode(response.body);
-    final Map<String, dynamic> bodyUser = body['user'];
-    print(body);
-    final String bodyToken = body['token'];
-    final UserModel user = UserModel(
-      userId: bodyUser['_id'],
-      firstName: bodyUser['firstName'],
-      lastName: bodyUser['lastName'],
-      userName: bodyUser['userName'],
-      email: bodyUser['email'],
-      password: bodyUser['password'],
-      userToken: bodyToken,
-      fcmToken: bodyUser['fcmToken'],
-      photoProfilePath: bodyUser['photoProfilePath'],
-      status: bodyUser['status'],
-    );
-    return user;
+    return response;
   }
 }
